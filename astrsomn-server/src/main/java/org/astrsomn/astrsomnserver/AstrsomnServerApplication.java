4de50cc6d0bab8aff74b531d6e29602df78c5180
香蕉你个啵娜娜 <l.h-p@qq.com>
2026-03-16 22:54:26 +0800

    feat: 初始化项目结构

--- astrsomn-server/src/main/java/org/astrsomn/astrsomnserver/AstrsomnServerApplication.java
+++ astrsomn-server/src/main/java/org/astrsomn/astrsomnserver/AstrsomnServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AstrsomnServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AstrsomnServerApplication.class, args);
     }
-
 }
---
+++ astrsomn-spring-boot-starter/src/main/java/org/astrsomn/astrsomnspringbootstarter/AstrsomnAutoConfiguration.java
@@ -0,0 +1,19 @@
+package org.astrsomn.astrsomnspringbootstarter;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@EnableConfigurationProperties(AstrsomnProperties.class)
+@ConditionalOnClass(name = "org.astrsomn.astrsomncore.service.AiService")
+public class AstrsomnAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AstrsomnProperties astrsomnProperties() {
+        return new AstrsomnProperties();
+    }
+}
