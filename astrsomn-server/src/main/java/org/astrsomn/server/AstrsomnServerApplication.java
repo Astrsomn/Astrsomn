72826fe7f9c701924a15854eda57353f70789103
香蕉你个啵娜娜 <l.h-p@qq.com>
2026-03-22 21:19:00 +0800

    feat: 修复依赖相关问题

--- astrsomn-server/src/main/java/org/astrsomn/server/AstrsomnServerApplication.java
+++ astrsomn-server/src/main/java/org/astrsomn/server/AstrsomnServerApplication.java
@@ -1,9 +1,11 @@
 package org.astrsomn.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.astrsomn.core.mapper")
 public class AstrsomnServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AstrsomnServerApplication.class, args);
--- astrsomn-spring-boot-starter/src/main/java/org/astrsomn/starter/config/AstrsomnAutoConfiguration.java
+++ astrsomn-spring-boot-starter/src/main/java/org/astrsomn/starter/config/AstrsomnAutoConfiguration.java
@@ -1,14 +1,12 @@
 package org.astrsomn.starter.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 @EnableConfigurationProperties(AstrsomnProperties.class)
-@ConditionalOnClass(name = "org.astrsomn.core.service.AiService")
 public class AstrsomnAutoConfiguration {
 
     @Bean
