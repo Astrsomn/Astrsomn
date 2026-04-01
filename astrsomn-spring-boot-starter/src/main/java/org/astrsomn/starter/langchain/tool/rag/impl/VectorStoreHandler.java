f39391ba4ba21385811b7b48cafe10218194055d
刘慧鹏 <l.h-p@qq.com>
2026-04-01 10:54:00 +0800

    ♻️ refactor(entity): 重构 RAG 实体为向量存储实体，统一命名规范

--- astrsomn-spring-boot-starter/src/main/java/org/astrsomn/starter/langchain/tool/rag/impl/VectorStoreHandler.java
+++ astrsomn-spring-boot-starter/src/main/java/org/astrsomn/starter/langchain/tool/rag/impl/VectorStoreHandler.java
@@ -3,7 +3,6 @@ package org.astrsomn.starter.langchain.tool.rag.impl;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import org.astrsomn.core.common.constant.VectorStoreType;
-import org.astrsomn.core.common.entity.RagKnowledgeEntity;
 import org.astrsomn.core.common.langchain.buildParam.setting.RagSetting;
 
 public interface VectorStoreHandler {
