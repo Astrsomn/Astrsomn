33d83b4f4a17c4138de532c8f9fb4fbde49273ba
香蕉你个啵娜娜 <l.h-p@qq.com>
2026-04-19 14:40:27 +0800

    🔥 refactor(工作流): 移除工作流引擎及相关组件，清理后端实体、服务、前端页面与节点组件

--- astrsomn-core/src/main/java/org/astrsomn/core/mapper/AiWorkflowMapper.java
+++
@@ -1,9 +0,0 @@
-package org.astrsomn.core.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.astrsomn.core.common.entity.AiWorkflowEntity;
-
-@Mapper
-public interface AiWorkflowMapper extends BaseMapper<AiWorkflowEntity> {
-}
